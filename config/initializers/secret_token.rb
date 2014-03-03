2fbdd1a9506eeb260165713e1a078f566ccb1c5a
Jesse Falzone <jesse.falzone@gmail.com>
2014-03-03 10:32:22 -0500

    New rails app.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+MapDemo::Application.config.secret_key_base = '553aa40671c1672cd55ba1babc0421191e289554a8e43e83f7115d3b82f49c617cba5813160380642e96da08664901dab64f22aba12e3f0b164d2b9fb4d37044'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+MapDemo::Application.config.session_store :cookie_store, key: '_map-demo_session'
